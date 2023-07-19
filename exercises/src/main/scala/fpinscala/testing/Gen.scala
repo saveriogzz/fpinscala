505375c46ee0a646605920b2483458d58c68df9a
Saverio Guzzo <saveriog@spotify.com>
2023-07-19 21:37:53 +0200

    Progressing on testing/Gen after long time

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -14,17 +14,34 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+  def check: Either[(FailedCase,SuccessCount), SuccessCount]
+//  def &&(p: Prop): Prop = new Prop {
+//    def check = Prop.this.check && p.check
+//  }
 }
 
 object Prop {
+  type SuccessCount = Int
+  type FailedCase = String
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] =
+    Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] =
+    Gen(State(RNG.boolean))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State(RNG.ints(n)))
 }
 
-trait Gen[A] {
+case class Gen[A](sample: State[RNG,A]) {
+  def choose(start: Int, stopExclusive:Int): Gen[Int] = {
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+  }
+
   def map[A,B](f: A => B): Gen[B] = ???
   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
 }
@@ -32,4 +49,3 @@ trait Gen[A] {
 trait SGen[+A] {
 
 }
-
